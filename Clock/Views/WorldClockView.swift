b9e856823450f2594ef820ca344739c40d5b879b
Savannah Ho Lung <sholung@lcs.on.ca>
2026-02-23 14:57:24 -0500

    Added toolbar items to the interface.

--- Clock/Views/WorldClockView.swift
+++ Clock/Views/WorldClockView.swift
@@ -12,6 +12,26 @@ struct WorldClockView: View {
         NavigationStack {
             Text("World Clock")
                 .navigationTitle("World Clock")
+                .toolbar {
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        
+                        Button("Edit") {
+                            // Does nothing right now
+                        }
+                        
+                    }
+                    
+                    ToolbarItem(placement: .primaryAction) {
+                        
+                        Button{
+                            // Does nothing right now
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        
+                    }
+                }
         }
     }
 }
