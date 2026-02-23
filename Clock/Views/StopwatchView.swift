6028fcfc065dcb413f4d36a1558a914474728cd6
Savannah Ho Lung <sholung@lcs.on.ca>
2026-02-23 14:37:17 -0500

    Got the tab view set up.

---
+++ Clock/Views/StopwatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopwatchView.swift
+//  Clock
+//
+//  Created by Savannah Ho Lung on 2026-02-23.
+//
+
+import SwiftUI
+
+struct StopwatchView: View {
+    var body: some View {
+        Text("Stopwatch")
+    }
+}
+
+#Preview {
+    StopwatchView()
+}
---
+++ Clock/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clock
+//
+//  Created by Savannah Ho Lung on 2026-02-23.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text("World Clock")
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
