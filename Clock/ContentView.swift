0ca92eb24c467f6c6c8c70343c2701302b619d9c
Savannah Ho Lung <sholung@lcs.on.ca>
2026-02-23 14:19:02 -0500

    Initial Commit

---
+++ Clock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clock
+//
+//  Created by Savannah Ho Lung on 2026-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
